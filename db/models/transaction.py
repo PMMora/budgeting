ac1e012bf6c4c62656ed46dc865d59d95472ea1d
pmmora <gpmora10@gmail.com>
2025-09-18 15:04:10 -0700

    refactor: remove dataclasses for now

--- db/models/transaction.py
+++ db/models/transaction.py
@@ -1,22 +1,13 @@
-
 from sqlalchemy import Column, Integer, String, Float, Date, Enum, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import date
 from base import Base
 from enums import TransactionType
-from dataclasses import dataclass
 
 
-@dataclass
-class TransactionObj:
-    id: int = None
-    type: TransactionType
-    amount: float = 0
-    category_id: int = None
-    vendor: str
-    note: str
-    
 class Transaction(Base):
+    __tablename__ = "transactions"
+    
     id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(Enum(TransactionType), nullable=False)
     amount = Column(Float, nullable=False)
@@ -24,6 +15,4 @@ class Transaction(Base):
     category = relationship("Category", back_populates="transactions")
     vendor = Column(String, nullable=True)
     note = Column(String, nullable=True)
-    date = Column(Date, default = date.today)
-    
-
+    date = Column(Date, default=date.today)
